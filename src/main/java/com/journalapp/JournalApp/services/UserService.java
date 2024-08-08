4241c22accbe891036a949e8dbd02d54b0c1d508
shubham <svats@youngman.co.in>
2024-08-08 11:33:12 +0530

    added authentication to user using spring securtiy

--- src/main/java/com/journalapp/JournalApp/services/UserService.java
+++ src/main/java/com/journalapp/JournalApp/services/UserService.java
@@ -1,20 +1,13 @@
 package com.journalapp.JournalApp.services;
 
 import com.journalapp.JournalApp.entities.User;
-import com.journalapp.JournalApp.repositories.JournalEntryRepository;
 import com.journalapp.JournalApp.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -26,29 +19,23 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    // No @Bean annotation here
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
-//    private final static Logger logger = LoggerFactory.getLogger(UserService.class);
-
-    public void saveUser(User user)
-    {
+    public void saveUser(User user) {
         userRepository.save(user);
     }
 
-    public boolean saveEntry(User user)
-    {
+    public boolean saveEntry(User user) {
         try{
+          user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("User"));
             userRepository.save(user);
             return true;
         }catch(Exception e){
+            System.out.println(e);
             return false;
-    }
-
-
+        }
     }
 
     public List<User> getAll()
